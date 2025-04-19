5bb3cfedba787128cc9c75d3a6e2dd1f397e4b85
Lidor Pahima <Lidor28@gmail.com>
2025-04-19 20:38:04 +0300

    imporve profile section

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,3 +7,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class VisitedCountry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='visited_countries')
+    country_name = models.CharField(max_length=100)
+    visit_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'country_name']  
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,)
-from .views import RegisterView , get_place_details_view ,   ProfileView
-from .views import plan_trip_view
+from .views import RegisterView , get_place_details_view ,   ProfileView,plan_trip_view, get_visited_countries, add_visited_country, remove_visited_country
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('plantrip/', plan_trip_view, name='plan_trip'),
     path('place-details/',get_place_details_view , name='place_details'),
-    path('profile/', ProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('api/visited-countries',get_visited_countries, name='visited_countries'),
+    path('api/visited-countries/<str:country_name>', remove_visited_country, name='remove_visited_country'),
+    path('api/visited-countries', add_visited_country, name='add_visited_country'),
 ]
