085b02e2ad41de38a08ada58f9c7d7307389e6b7
Lidor Pahima <Lidor28@gmail.com>
2025-04-27 10:24:05 +0300

    Updated Settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,21 +45,10 @@ CORS_ALLOWED_ORIGINS = os.getenv('DJANGO_CORS_ALLOWED_ORIGINS', '').split(',') i
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'production')
 POSTGRES_LOCALLY = os.getenv('POSTGRES_LOCALLY', 'False') == 'True'
 
-if ENVIRONMENT == 'production' or POSTGRES_LOCALLY:
-    DATABASES = {
-        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('POSTGRES_DB'),
-            'USER': os.getenv('POSTGRES_USER'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-            'HOST': os.getenv('POSTGRES_HOST'),
-            'PORT': os.getenv('POSTGRES_PORT', '5432'),
-        }
-    }
+# Database configuration
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+}
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -113,11 +102,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 
-REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
-REDIS_DB = int(os.getenv('REDIS_DB', 0))
-REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
-REDIS_URL = os.getenv('REDIS_URL', f"redis://{':' + REDIS_PASSWORD + '@' if REDIS_PASSWORD else ''}{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}")
+# Redis configuration
+REDIS_URL = os.getenv('REDIS_URL')
 
 
 # Password validation
